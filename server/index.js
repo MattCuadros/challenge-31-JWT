b38e6cce6cb1755a82ff3090ccd6b0a67c7c2104
Matt Cuadros <mcuadrose@gmail.com>
2023-06-21 11:23:15 -0400

    post/login added

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import  jwt  from "jsonwebtoken";
 import { pool } from "./db/query.js";
 import morgan from "morgan";
 import { handleErrors } from "./db/handleErrors.js";
@@ -23,7 +24,6 @@ app.post("/usuarios", async (req, res) => {
   const { email, password, role, language } = req.body;
   console.log(req.body);
   try {
-    /* Inserción SQL */
     if (!email || !password || !role || !language) {
       throw res.json({
         ok: false,
@@ -41,8 +41,26 @@ app.post("/usuarios", async (req, res) => {
       });
     }
   } catch (error) {
-    console.log(error);
     const { status, message } = handleErrors(error.code);
     return res.status(status).json({ ok: false, message });
   }
 });
+
+
+app.post("/login", async (req, res)=>{
+  const {email, password}=req.body;
+  console.log(req.body);
+  try {
+    if(!email||!password){
+      throw res.json({
+        ok: false,
+        msg: "Ingrese todos los datos",
+      });
+    } 
+    const token=jwt.sign({email},process.env.SECRET_KEY);
+    res.json({token})
+  } catch (error) {
+    const { status, message } = handleErrors(error.code);
+    return res.status(status).json({ ok: false, message });
+  }
+})
